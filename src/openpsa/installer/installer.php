0cd90e351469540571ffad790023907264b02199
Andreas Flack <flack@contentcontrol-berlin.de>
2013-05-17 18:16:32 +0200

    Initial setup

---
+++ src/openpsa/installer/installer.php
@@ -0,0 +1,14 @@
+<?php
+namespace openpsa\installer;
+use Composer\Installer as base_installer
+
+class installer extends base_installer
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function supports($packageType)
+    {
+        return $packageType === 'midcom-extras';
+    }
+}
