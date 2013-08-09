4b976274779039c9008bdf363c48ea6d76ba50ec
Andreas Flack <flack@contentcontrol-berlin.de>
2013-08-09 12:14:43 +0200

    Add basic linker test

---
+++ test/linkerTest.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @package openpsa.installer
+ * @author CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @copyright CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ */
+
+use openpsa\installer\linker;
+use Composer\Util\Filesystem;
+use Composer\IO\IOInterface;
+
+/**
+ * Simple linker tests
+ *
+ * @package openpsa.installer
+ */
+class linkerTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var string
+     */
+    protected $basedir;
+
+    /**
+     * @var Composer\Util\Filesystem
+     */
+    protected $fs;
+
+    /**
+     * @var Composer\IO\IOInterface
+     */
+    protected $io;
+
+    protected function setUp()
+    {
+        $this->basedir = realpath(sys_get_temp_dir()) . DIRECTORY_SEPARATOR . 'test-basedir';
+        $this->io = $this->getMock('Composer\IO\IOInterface');
+
+        $this->fs = new Filesystem;
+        $this->fs->ensureDirectoryExists($this->basedir);
+        $this->fs->ensureDirectoryExists($this->basedir . DIRECTORY_SEPARATOR . 'static');
+        $this->fs->ensureDirectoryExists($this->basedir . DIRECTORY_SEPARATOR . 'static' . DIRECTORY_SEPARATOR . 'component.name');
+        $this->fs->ensureDirectoryExists($this->basedir . DIRECTORY_SEPARATOR . 'themes');
+        $this->fs->ensureDirectoryExists($this->basedir . DIRECTORY_SEPARATOR . 'themes' . DIRECTORY_SEPARATOR . 'theme-name');
+        $this->fs->ensureDirectoryExists($this->basedir . DIRECTORY_SEPARATOR . 'themes' . DIRECTORY_SEPARATOR . 'theme-name' . DIRECTORY_SEPARATOR . 'static');
+    }
+
+    protected function tearDown()
+    {
+        $this->fs->removeDirectory($this->basedir);
+    }
+
+    public function testInstall()
+    {
+        $linker = new linker($this->basedir, $this->io);
+        $linker->install($this->basedir);
+        $this->assertFileExists($this->basedir . DIRECTORY_SEPARATOR . 'web');
+        $this->assertFileExists($this->basedir . DIRECTORY_SEPARATOR . 'web' . DIRECTORY_SEPARATOR . 'midcom-static');
+        $this->assertFileExists($this->basedir . DIRECTORY_SEPARATOR . 'web' . DIRECTORY_SEPARATOR . 'midcom-static' . DIRECTORY_SEPARATOR . 'component.name');
+        $this->assertFileExists($this->basedir . DIRECTORY_SEPARATOR . 'web' . DIRECTORY_SEPARATOR . 'midcom-static' . DIRECTORY_SEPARATOR . 'theme-name');
+    }
+}
